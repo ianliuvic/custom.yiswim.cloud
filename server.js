c44c1562a0575325f2db49bbb968ea23e9052323
ianliuvic <ian.liu.vic@gmail.com>
2026-03-12 18:28:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set('trust proxy', 1);
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 分钟
     max: 5, // 限制 5 次
-    message: { success: false, message: "登录尝试过于频繁，请15分钟后再试。" },
+    message: { success: false, message: "请求过于频繁，请15分钟后再试。" },
     standardHeaders: true, // 在响应头中显示剩余次数
     legacyHeaders: false,
 });

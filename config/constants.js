ff75ebe154df8550add1ce739510b95928a2ef99
ianliuvic <ian.liu.vic@gmail.com>
2026-04-01 09:29:47 +0800

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key-123456';
-const N8N_BASE_URL = 'http://n8n-ywock00sw4ko80c4w4ogs8so:5678/webhook-test';
+const N8N_BASE_URL = 'http://n8n-ywock00sw4ko80c4w4ogs8so:5678/webhook';
 
 module.exports = { JWT_SECRET, N8N_BASE_URL };
